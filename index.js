1723c7886b9fc6f7815dc8d2858278551ae7d587
Mercy Cherono <cheronom142@gmail.com>
2023-10-02 03:24:25 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+// Setting up a DOMContentLoaded event listener
+document.addEventListener("DOMContentLoaded", function () {
+  //Target the paragraph element with the id "text"
+  let text = document.getElementById("text");
+
+  //Replace the text in the element
+
+  text.textContent = "This is really cool!";
+});
